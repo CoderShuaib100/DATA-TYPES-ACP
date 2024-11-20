7499fce1b283e6587a964586c64942bfd1c6fb63
CoderShuaib100 <sayeedishuaib@gmail.com>
2024-11-20 19:16:07 +0530

    first commit

---
+++ CONGRATULATOR.py
@@ -0,0 +1,9 @@
+# start
+print("Congratulation Message Generator.")
+# getting the name
+name = input("Enter your name: ")
+print("Write the reason \n Do not write 'for' like 'for winning' only 'winning' but if your reason has any other conjunctions like being,becoming,using please write that also.")
+reason = input("For what purpose do you want to congratulate him/her: ")
+print("Hi",name,",Congratulations for",reason)
+print("Done! \n Thank you for using Congratulator Python")
+
